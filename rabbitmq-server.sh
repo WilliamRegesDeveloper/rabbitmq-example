1fc7de1d7f5ca6fbcc103124b96d1c1f1da9c2d5
william reges <william.reges1986@gmail.com>
2020-09-02 00:49:43 -0300

    example run cluster docker

---
+++ rabbitmq-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#server rabbitmq example for developer
+docker run -d -p 15672:15672 -p 5672:5672 --hostname some-rabbit --name some-rabbit -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=password rabbitmq:3-management
+
