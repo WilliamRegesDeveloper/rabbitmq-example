d96e1951713ad4705880d141e24830c9bf8fa493
william reges <william.reges1986@gmail.com>
2020-09-02 01:26:24 -0300

    ajuste em cluster

--- cluster-example/cluster-run.sh
+++ cluster-example/cluster-run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker rm -f some-rabbit
-docker rmi -f instant-payment:0.0.1
-docker rmi -f instant-credit:0.0.1
-mvn -f ../instant-payment/pom.xml clean package
-mvn -f ../instant-credit/pom.xml clean package
+docker rmi -f producer-payment:0.0.1
+docker rmi -f consumer-payment:0.0.1
+mvn -f ../producer-payment/pom.xml clean package
+mvn -f ../consumer-payment/pom.xml clean package
 docker-compose up -d
 
 
